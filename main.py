218c7b687032b4ab6b2fdc163d7c79b5feb09a09
MasamioNakada <mnakadab@unac.edu.pe>
2022-07-12 19:54:54 -0500

    automated

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from utils import Load
 from resumen import dict_resume
+from export import export_csv
 
-path = './data/e-comerce_Olist_dataset'
+path = "./data/e-comerce_Olist_dataset"
 load = Load(path)
 
 data_dict = load.load_from_csv(path)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    dict_resume(data_dict)
-
-    
+    # dict_resume(data_dict)
+    export_csv(data_dict, "./out")
