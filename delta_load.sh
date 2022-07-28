8c922c8f309581410d7809cdb2d4de24d97a9b79
MasamioNakada <mnakadab@unac.edu.pe>
2022-07-28 02:06:19 -0500

    prueba

--- delta_load.sh
+++ delta_load.sh
@@ -4,11 +4,5 @@ python delta.py
 git pull
 git add .
 git commit -m"${message}"
-if [ -n "$(git status - porcelain)" ];
-then
- echo "IT IS CLEAN"
-else
- git status
- echo "Pushing data to remote server!!!"
- git push -u origin main
-fi
+git status
+git push origin main
