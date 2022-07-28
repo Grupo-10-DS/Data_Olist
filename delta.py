dfa24ec6382548e1bc562a2450f811748a816673
Sebastian Aguirre <sebasaguirresolano@gmail.com>
2022-07-28 00:33:55 -0500

    FIX action

--- delta.py
+++ delta.py
@@ -1,17 +1,16 @@
 from wikiframe import Extractor
 
-from validators import find_date,col_lower,col_upp, dupli_id, date_order_val
+from validators import find_date, col_lower, col_upp, dupli_id, date_order_val
 from load import load_csv
 
-general_transform = [find_date,col_lower,col_upp,dupli_id,date_order_val]
+general_transform = [find_date, col_lower, col_upp, dupli_id, date_order_val]
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    extract = Extractor('Dataset_sinteticos')
+    extract = Extractor("Dataset_sinteticos")
 
-    #extrac and Transform Delta
-    delta_dict = extract.extract_from_csv(func=general_transform,verbose=False)
+    # extrac and Transform Delta
+    delta_dict = extract.extract_from_csv(func=general_transform, verbose=False)
 
-    #Export
-    load_csv(delta_dict,'Dataset_delta')
-    
+    # Export
+    load_csv(delta_dict, "Dataset_delta")
